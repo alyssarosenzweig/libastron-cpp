e0246287cdb6e75872decb3a7744cf70a50a0e40
bobbybee <j104@rocketmail.com>
2014-11-10 08:54:50 -0500

    Use new msgtypes

--- test_air.cpp
+++ test_air.cpp
@@ -1,12 +1,13 @@
 #include <bamboo/wire/Datagram.h>
 #include <AIRepository.h>
+#include <msgtypes.h>
 
 int main() {
 	boost::asio::io_service io_service;
 	AIRepository repo(&io_service, "localhost", 7199, "simple_example.dc");
 
 	Datagram dg;
-	repo.control_header(&dg, 9012);
+	repo.control_header(&dg, CONTROL_SET_CON_NAME);
 	dg.add_string("libastron-c++");
 	repo.send(dg);
 
