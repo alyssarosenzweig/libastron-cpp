d5ad1702d6d460e71c490dbc0d5ff8334041e207
bobbybee <j104@rocketmail.com>
2014-11-11 15:37:43 -0500

    Fix a problem where AIWatcher::message would not be called

--- AIWatcher.h
+++ AIWatcher.h
@@ -4,15 +4,13 @@
 #include "global.h"
 #include "ChannelWatcher.h"
 
-class AIRepository;
-
 class AIWatcher : public ChannelWatcher {
 public:
 	AIWatcher(uint64_t airId) : ChannelWatcher(airId) {
-
+		cout << "AIWatcher found on airId " << airId << endl;
 	};
 
-	void message(AIRepository* cr, DatagramIterator* di, uint64_t sender, uint16_t msgtype);
+	void message(ConnectionRepository* cr, DatagramIterator* di, uint64_t sender, uint16_t msgtype);
 };
 
 #endif
