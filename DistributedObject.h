ff1c493b5e6636d7055a895baa67033d60f3e81e
bobbybee <j104@rocketmail.com>
2014-11-10 19:51:21 -0500

    LoginManager class to illustrate new features

--- DistributedObject.h
+++ DistributedObject.h
@@ -9,7 +9,7 @@ public:
 	DistributedObject(uint32_t do_id);
 	void message(ConnectionRepository* cr, DatagramIterator* dg, uint64_t sender, uint16_t msgtype);
 	virtual void fieldUpdate(string fieldName, vector<Value> arguments) {
-		
+		printf("HFDGJYHGNM\n");
 	};
 private:
 	uint32_t m_do_id;
--- test_air.cpp
+++ test_air.cpp
@@ -3,6 +3,14 @@
 #include <msgtypes.h>
 #include <DistributedObject.h>
 
+class LoginManager : public DistributedObject {
+public:
+	LoginManager(uint64_t do_id) : DistributedObject(do_id) {};
+	void fieldUpdate(string fieldName, vector<Value> arguments) {
+		cout << "LoginManager update on field " << fieldName << endl;
+	}
+};
+
 int main() {
 	boost::asio::io_service io_service;
 	AIRepository repo(&io_service, "localhost", 7199, "simple_example.dc");
@@ -12,7 +20,7 @@ int main() {
 	dg.add_string("libastron-c++");
 	repo.send(dg);
 
-	DistributedObject loginWatcher(1234);
+	LoginManager loginWatcher(1234);
 
 	repo.subscribe_channel(&loginWatcher);
 
