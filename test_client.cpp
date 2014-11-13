bc217588a932a44e4cae58ecaf51fd67159f8c79
bobbybee <j104@rocketmail.com>
2014-11-13 13:34:59 -0500

    Subclass varstring because Bamboo is premature

--- test_client.cpp
+++ test_client.cpp
@@ -1,4 +1,29 @@
 #include <ClientRepository.h>
+#include <DistributedObject.h>
+#include <global.h>
+
+class Varstring : public Type {
+public:
+	Varstring() : Type() {
+		m_subtype = kTypeVarstring;
+	}
+};
+
+class LoginManager : public DistributedObject {
+public:
+	LoginManager(uint64_t do_id) : DistributedObject(do_id) {};
+	string classname() { return "LoginManager"; };
+
+	void sendLogin(string username, string password) {
+		Value* username_ = new Value(new Varstring());
+		Value* password_ = new Value(new Varstring());
+
+		username_->string_ = username;
+		password_->string_ = password;
+
+		sendUpdate("login", vector<Value*>{username_, password_});
+	}
+};
 
 int main() {
 	boost::asio::io_service io_service;
