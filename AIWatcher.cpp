2d8801bced9831110a127a3aeb8a421b7e2c789a
bobbybee <j104@rocketmail.com>
2014-11-13 09:05:06 -0500

    AIWatcher.cpp

---
+++ AIWatcher.cpp
@@ -0,0 +1,21 @@
+#include "AIWatcher.h"
+#include "AIRepository.h"
+#include "msgtypes.h"
+
+void AIWatcher::message(ConnectionRepository* _cr, DatagramIterator* di, uint64_t sender, uint16_t msgtype) {
+	cout << "AIWatcher message type " << msgtype << endl;
+
+	AIRepository* cr = (AIRepository*) _cr;
+
+	switch(msgtype) {
+		case STATESERVER_OBJECT_SET_FIELD: {
+			cr->set_message_sender(sender);
+			cr->handleSetField(di);
+			break;
+		};
+		case STATESERVER_OBJECT_ENTER_AI_WITH_REQUIRED: {
+			cr->handleEnterAi(di);
+			break;
+		}
+	}
+}
