16ed68d6d2cfba59981de41deb370a3af00acb2b
bobbybee <j104@rocketmail.com>
2014-11-13 13:50:10 -0500

    I have just developed a slight slight hatred for Bamboo

--- test_client.cpp
+++ test_client.cpp
@@ -1,13 +1,9 @@
 #include <ClientRepository.h>
 #include <DistributedObject.h>
 #include <global.h>
-
-class Varstring : public Type {
-public:
-	Varstring() : Type() {
-		m_subtype = kTypeVarstring;
-	}
-};
+#include <bamboo/module/Numeric.h>
+#include <bamboo/module/NumericRange.h>
+#include <bamboo/module/Array.h>
 
 class LoginManager : public DistributedObject {
 public:
@@ -15,21 +11,25 @@ public:
 	string classname() { return "LoginManager"; };
 
 	void sendLogin(string username, string password) {
-		Value* username_ = new Value(new Varstring());
-		Value* password_ = new Value(new Varstring());
-
+		NumericRange* range = new NumericRange(0, 65535);
+		Array* stringType = new Array(new Numeric(kTypeChar), *range);
+		Value* username_ = new Value(stringType);
+		Value* password_ = new Value(stringType);
 		username_->string_ = username;
 		password_->string_ = password;
-
 		sendUpdate("login", vector<Value*>{username_, password_});
 	}
 };
+
+LoginManager loginManager(1234);
 
 int main() {
 	boost::asio::io_service io_service;
 	ClientRepository repo(&io_service, "localhost", 7198, "simple_example.dc", "SimpleExample v0.2", 
 		[]{
+			loginManager.sendLogin("guest", "guest");
 			cout << "hello response" << endl;
+
 		});
 
 	repo.loop();
